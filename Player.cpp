860554258a70a341cb38f72eb0aa490d5d554202
GeorgeBilias <93704930+GeorgeBilias@users.noreply.github.com>
2022-01-09 18:01:12 +0200

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,9 +1,11 @@
 #include "Player.h"
 
-Player::Player()
+Player::Player(Deck p_deck)
 {
 	this->money = 10000;
-	
+	CardState = true;
+	this->player_deck = p_deck;
+	this->pd_deck.first_cards(p_deck.giveCard(), p_deck.giveCard());
 }
 
 Player::~Player()
@@ -28,4 +30,9 @@ void Player::setPosX(float x)
 void Player::setPosY(float y)
 {
 	this->position[1] = y;
+}
+
+int Player::getScore() const
+{
+	return score;
 }
--- Player.h
+++ Player.h
@@ -9,13 +9,15 @@ protected:
 	Deck player_deck;
 	Player_deck pd_deck;
 	bool CardState;
+	int score;
 public:
-	Player();
+	Player(Deck p_deck);
 	~Player();
 	float GetPosX() const;
 	float GetPosY() const;
 	void setPosX(float x);
 	void setPosY(float y);
+	int getScore() const;
 private:
 	float position[2];
 	int money;
